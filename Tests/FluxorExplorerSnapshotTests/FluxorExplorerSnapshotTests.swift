f161f3750699661f20b9a908a9b20e42dcfd9f0c
Morten Bjerg Gregersen <morten@mogee.dk>
2019-12-21 14:05:47 +0100

    Add old state (#5)
    * Add old state
    
    * Update Package.resolved

--- Tests/FluxorExplorerSnapshotTests/FluxorExplorerSnapshotTests.swift
+++ Tests/FluxorExplorerSnapshotTests/FluxorExplorerSnapshotTests.swift
@@ -6,38 +6,47 @@ import XCTest
 final class FluxorExplorerSnapshotTests: XCTestCase {
     let action = TestAction(increment: 1)
     let otherAction = OtherTestAction()
-    let state = State()
-    let otherState = State(count: 1337)
+    let oldState = State(count: 1)
+    let otherOldState = State(count: 2)
+    let newState = State(count: 3)
+    let otherNewState = State(count: 4)
     let date = Date(timeIntervalSince1970: 1576706397)
-    let json = #"{"date":598399197,"action":{"name":"TestAction","payload":{"increment":1}},"newState":{"count":42}}"#
+    let json = #"{"action":{"name":"TestAction","payload":{"increment":1}},"newState":{"count":3},"oldState":{"count":1},"date":598399197}"#
 
     func testEqual() throws {
-        let snapshot1 = FluxorExplorerSnapshot(action: action, newState: state, date: date)
-        let snapshot2 = FluxorExplorerSnapshot(action: action, newState: state, date: date)
+        let snapshot1 = FluxorExplorerSnapshot(action: action, oldState: oldState, newState: newState, date: date)
+        let snapshot2 = FluxorExplorerSnapshot(action: action, oldState: oldState, newState: newState, date: date)
         XCTAssertTrue(snapshot1 == snapshot2)
     }
 
     func testNotEqualByAction() throws {
-        let snapshot1 = FluxorExplorerSnapshot(action: action, newState: state, date: date)
-        let snapshot2 = FluxorExplorerSnapshot(action: otherAction, newState: state, date: date)
+        let snapshot1 = FluxorExplorerSnapshot(action: action, oldState: oldState, newState: newState, date: date)
+        let snapshot2 = FluxorExplorerSnapshot(action: otherAction, oldState: oldState, newState: newState, date: date)
         XCTAssertFalse(snapshot1 == snapshot2)
     }
 
-    func testNotEqualByState() throws {
-        let snapshot1 = FluxorExplorerSnapshot(action: action, newState: state, date: date)
-        let snapshot2 = FluxorExplorerSnapshot(action: action, newState: otherState, date: date)
+    func testNotEqualByOldState() throws {
+        let snapshot1 = FluxorExplorerSnapshot(action: action, oldState: oldState, newState: newState, date: date)
+        let snapshot2 = FluxorExplorerSnapshot(action: action, oldState: otherOldState, newState: newState, date: date)
+        XCTAssertFalse(snapshot1 == snapshot2)
+    }
+
+    func testNotEqualByNewState() throws {
+        let snapshot1 = FluxorExplorerSnapshot(action: action, oldState: oldState, newState: newState, date: date)
+        let snapshot2 = FluxorExplorerSnapshot(action: action, oldState: oldState, newState: otherNewState, date: date)
         XCTAssertFalse(snapshot1 == snapshot2)
     }
 
     func testInitWithActionAndState() throws {
-        let snapshot = FluxorExplorerSnapshot(action: action, newState: state, date: date)
+        let snapshot = FluxorExplorerSnapshot(action: action, oldState: oldState, newState: newState, date: date)
         XCTAssertEqual(snapshot.actionData.name, "TestAction")
         XCTAssertEqual(snapshot.actionData.payload, ["increment": AnyCodable(action.increment)])
-        XCTAssertEqual(snapshot.newState, ["count": AnyCodable(state.count)])
+        XCTAssertEqual(snapshot.oldState, ["count": AnyCodable(oldState.count)])
+        XCTAssertEqual(snapshot.newState, ["count": AnyCodable(newState.count)])
     }
 
     func testEncode() throws {
-        let snapshot = FluxorExplorerSnapshot(action: action, newState: state, date: date)
+        let snapshot = FluxorExplorerSnapshot(action: action, oldState: oldState, newState: newState, date: date)
         let data = try JSONEncoder().encode(snapshot)
         XCTAssertEqual(String(data: data, encoding: .utf8), json)
     }
@@ -45,7 +54,7 @@ final class FluxorExplorerSnapshotTests: XCTestCase {
     func testDecode() throws {
         let data = json.data(using: .utf8)!
         let snapshot = try JSONDecoder().decode(FluxorExplorerSnapshot.self, from: data)
-        let expectedSnapshot = FluxorExplorerSnapshot(action: action, newState: state, date: date)
+        let expectedSnapshot = FluxorExplorerSnapshot(action: action, oldState: oldState, newState: newState, date: date)
         XCTAssertEqual(snapshot, expectedSnapshot)
     }
 }
