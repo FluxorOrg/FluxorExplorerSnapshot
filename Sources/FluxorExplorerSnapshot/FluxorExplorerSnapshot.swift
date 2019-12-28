9b7a1eae1adcdc0bd9b121baa12baef46698a4c0
Morten Bjerg Gregersen <morten@mogee.dk>
2019-12-28 23:08:34 +0100

    Always get payload from Action

--- Sources/FluxorExplorerSnapshot/FluxorExplorerSnapshot.swift
+++ Sources/FluxorExplorerSnapshot/FluxorExplorerSnapshot.swift
@@ -8,12 +8,16 @@ public struct FluxorExplorerSnapshot: Codable, Equatable {
     public let newState: [String: AnyCodable]
     public internal(set) var date: Date
 
-    public init<State: Encodable>(action: Action, oldState: State, newState: State, date: Date = .init()) {
-        self.actionData = .init(name: String(describing: type(of: action)), payload: action.encodablePayload)
-        let encodedOldState = try! JSONEncoder().encode(oldState)
-        self.oldState = try! JSONDecoder().decode([String: AnyCodable].self, from: encodedOldState)
-        let encodedNewState = try! JSONEncoder().encode(newState)
-        self.newState = try! JSONDecoder().decode([String: AnyCodable].self, from: encodedNewState)
+    public init<A: Action, State: Encodable>(action: A, oldState: State, newState: State, date: Date = .init()) {
+        let encoder = JSONEncoder()
+        let decoder = JSONDecoder()
+        let encodedAction = try! encoder.encode(action)
+        let actionPayload = try! decoder.decode([String: AnyCodable].self, from: encodedAction)
+        self.actionData = .init(name: String(describing: type(of: action)), payload: actionPayload)
+        let encodedOldState = try! encoder.encode(oldState)
+        self.oldState = try! decoder.decode([String: AnyCodable].self, from: encodedOldState)
+        let encodedNewState = try! encoder.encode(newState)
+        self.newState = try! decoder.decode([String: AnyCodable].self, from: encodedNewState)
         self.date = date
     }
 
@@ -27,10 +31,5 @@ public struct FluxorExplorerSnapshot: Codable, Equatable {
     public struct ActionData: Codable, Equatable {
         public let name: String
         public let payload: [String: AnyCodable]?
-
-        init(name: String, payload: [String: AnyEncodable]?) {
-            self.name = name
-            self.payload = payload?.mapValues { AnyCodable($0.value) }
-        }
     }
 }
